a7c7992af336343b64528f239f5521bd3b492a1e
joaquinlechesi <joaquinlechesi@gmail.com>
2025-06-17 12:48:20 -0300

    Filtrar por estado

---
+++ Tareas/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+using Tareas;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("\nConsumo de API.");
+
+HttpClient client = new HttpClient();
+
+string url = "https://jsonplaceholder.typicode.com/todos/";
+HttpResponseMessage response = await client.GetAsync(url);
+response.EnsureSuccessStatusCode();
+
+string responseBody = await response.Content.ReadAsStringAsync();
+List<Tarea> tareas = JsonSerializer.Deserialize<List<Tarea>>(responseBody);
+Console.WriteLine("TAREAS NO COMPLETADAS");
+foreach (var tarea in tareas)
+{
+    if (!tarea.completed)
+    {
+        Console.WriteLine($"TITULO: {tarea.title} - ESTADO: {tarea.completed}");
+    }
+}
+Console.WriteLine("TAREAS COMPLETADAS");
+foreach (var tarea in tareas)
+{
+    if (tarea.completed)
+    {
+        Console.WriteLine($"TITULO: {tarea.title} - ESTADO: {tarea.completed}");
+    }
+}
---
+++ Tareas/Tarea.cs
@@ -0,0 +1,9 @@
+namespace Tareas;
+
+public class Tarea
+{
+    public int userId { get; set; }
+    public int id { get; set; }
+    public string title { get; set; }
+    public bool completed { get; set; }
+}
