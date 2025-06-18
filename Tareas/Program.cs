3c81c08d3bcc265d11114348487f2bf173d7a0a4
joaquinlechesi <joaquinlechesi@gmail.com>
2025-06-17 21:51:43 -0300

    Creacion de tareas.json

--- Tareas/Program.cs
+++ Tareas/Program.cs
@@ -19,11 +19,24 @@ foreach (var tarea in tareas)
         Console.WriteLine($"TITULO: {tarea.title} - ESTADO: {tarea.completed}");
     }
 }
+
+List<Tarea> tareasCompletadas = new List<Tarea>();
 Console.WriteLine("TAREAS COMPLETADAS");
 foreach (var tarea in tareas)
 {
+    //Tarea nuevaTarea = new Tarea(tarea.userId, tarea.id, tarea.title, tarea.completed);
     if (tarea.completed)
     {
+        Tarea nuevaTarea = new Tarea { userId = tarea.id, id = tarea.id, title = tarea.title, completed = tarea.completed };
+        tareasCompletadas.Add(nuevaTarea);
         Console.WriteLine($"TITULO: {tarea.title} - ESTADO: {tarea.completed}");
     }
 }
+string jsonString = JsonSerializer.Serialize<List<Tarea>>(tareasCompletadas);
+
+// // Crear archivo
+string nombreArchivo = "tareas.json";
+string ruta = "C:\\Users\\joaqu\\Facultad\\2025\\Taller de Lenguajes I\\Semana_10\\tl1-tp10-2025-joaquinlechesi\\Tareas\\";
+StreamWriter nuevoArchivo = new StreamWriter(ruta + nombreArchivo);
+nuevoArchivo.WriteLine(jsonString);
+nuevoArchivo.Close();
--- Tareas/Tarea.cs
+++ Tareas/Tarea.cs
@@ -6,4 +6,11 @@ public class Tarea
     public int id { get; set; }
     public string title { get; set; }
     public bool completed { get; set; }
+    // public Tarea(int userId_const, int id_const, string title_const, bool completed_const)
+    // {
+    //     this.userId = userId_const;
+    //     this.id = id_const;
+    //     this.title = title_const;
+    //     this.completed = completed_const;
+    // }
 }
