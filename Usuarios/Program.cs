02ee8b41a247656f90aa2b601b317fee15d4b35c
joaquinlechesi <joaquinlechesi@gmail.com>
2025-06-22 17:30:21 -0300

    Agregando detalles faltantes a la clase Usuario

--- Usuarios/Program.cs
+++ Usuarios/Program.cs
@@ -13,7 +13,12 @@ response.EnsureSuccessStatusCode(); //Metodo del objeto de respuesta (HttpRespon
 string responseBody = await response.Content.ReadAsStringAsync(); //Metodo del contenido de la respuesta (HttpClient) para leer el cuerpo del mensaje como un string
 List<Usuario> usuarios = JsonSerializer.Deserialize<List<Usuario>>(responseBody);
 Console.WriteLine("RESULTADOS:");
+int contador = 1; //Uso el "contador" en lugar de "usuario.id" por si los numeros de usuarios no vienen ordenados
 foreach (var usuario in usuarios)
 {
-    Console.WriteLine($"{usuario.address.street}");
+    if (contador < 6)
+    {
+        Console.WriteLine($"NONBRE: {usuario.name} - CORREO ELECTRONICO: {usuario.email} - DOMICILIO: {usuario.address.street} - {usuario.address.city}");
+        contador++;
+    }
 }
--- Usuarios/Usuario.cs
+++ Usuarios/Usuario.cs
@@ -9,7 +9,7 @@ public class Usuario
     public string username { get; set; }
     public string email { get; set; }
     public Address address { get; set; }
-    public string street { get; set; }
+    //public string street { get; set; }
     public string phone { get; set; }
     public string website { get; set; }
     public Company company { get; set; }
@@ -19,11 +19,17 @@ public class Address
     public string street { get; set; }
     public string suite { get; set; }
     public string city { get; set; }
-
+    public Geo geo { get; set; }
 }
 public class Company
 {
     public string name { get; set; }
     public string catchPhrase { get; set; }
     public string bs { get; set; }
+}
+
+public class Geo
+{
+    public string lat { get; set; }
+    public string lng { get; set; }
 }
