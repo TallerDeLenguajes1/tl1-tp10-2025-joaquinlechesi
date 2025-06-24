337bb5d55ec8474c0a41df95a31418fc10506918
joaquinlechesi <joaquinlechesi@gmail.com>
2025-06-24 15:45:40 -0300

    Implementado Colection<T>.Take

--- Usuarios/Program.cs
+++ Usuarios/Program.cs
@@ -13,18 +13,24 @@ response.EnsureSuccessStatusCode(); //Metodo del objeto de respuesta (HttpRespon
 string responseBody = await response.Content.ReadAsStringAsync(); //Metodo del contenido de la respuesta (HttpClient) para leer el cuerpo del mensaje como un string
 List<Usuario> usuarios = JsonSerializer.Deserialize<List<Usuario>>(responseBody);
 Console.WriteLine("RESULTADOS:");
-int contador = 1; //Uso el "contador" en lugar de "usuario.id" por si los numeros de usuarios no vienen ordenados
-foreach (var usuario in usuarios)
+//int contador = 1; //Uso el "contador" en lugar de "usuario.id" por si los numeros de usuarios no vienen ordenados
+var aux = usuarios.Take(5); //toma a lo sumo los primeros 5 lementos o menos
+foreach (var usuario in aux)
 {
-    if (contador < 6)
-    {
-        Console.WriteLine($"NONBRE: {usuario.name} - CORREO ELECTRONICO: {usuario.email} - DOMICILIO: Calle: {usuario.address.street} - Ciudad: {usuario.address.city}");
-        //Console.WriteLine($"{usuario.id} {usuario.name} {usuario.username} {usuario.email}");
-        //Console.WriteLine($"{usuario.address.street} {usuario.address.suite} {usuario.address.city} {usuario.address.zipcode}");
-        //Console.WriteLine($"{usuario.address.geo.lat} {usuario.address.geo.lng}");
-        //Console.WriteLine($"{usuario.phone} {usuario.website}");
-        //Console.WriteLine($"{usuario.company.name} {usuario.company.catchPhrase} {usuario.company.bs}");
-        contador++;
-    }
+    Console.WriteLine($"NONBRE: {usuario.name} - CORREO ELECTRONICO: {usuario.email} - DOMICILIO: Calle: {usuario.address.street} - Ciudad: {usuario.address.city}");
+    //if (contador < 6)
+    //{
+    //    //Console.WriteLine($"{usuario.id} {usuario.name} {usuario.username} {usuario.email}");
+    //    //Console.WriteLine($"{usuario.address.street} {usuario.address.suite} {usuario.address.city} {usuario.address.zipcode}");
+    //    //Console.WriteLine($"{usuario.address.geo.lat} {usuario.address.geo.lng}");
+    //    //Console.WriteLine($"{usuario.phone} {usuario.website}");
+    //    //Console.WriteLine($"{usuario.company.name} {usuario.company.catchPhrase} {usuario.company.bs}");
+    //    contador++;
+    //}
+    //else
+    //{
+    //    break;
+    //|}
 }
+
 Console.WriteLine("Fin del programa.");
